1d6d6144c23d4967dda89f7f5917b9e0da46c12b
Will Larry <will23larry@gmail.com>
2021-05-06 16:53:08 -0400

    Refactor app to handle Apollo setup.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,48 @@
+// import { ApolloProvider } from '@apollo/client';
+// import '../styles/globals.css'
+// import Page from 'components/Page';
+// import withData from '../lib/utils/withData'
+
+// function MyApp({ Component, pageProps, err, apollo }) {
+//   console.log(apollo);
+  
+//   return (
+//     <ApolloProvider client={apollo}>
+//       <Page backgroundColor={pageProps?.backgroundColor}>
+//         <Component err={err} {...pageProps} />
+//       </Page>
+//     </ApolloProvider>
+
+//   )
+// }
+
+// MyApp.getInitialProps = async function ({ Component, ctx }) {
+//   let pageProps = {};
+//   if (Component.getInitialProps) {
+//     pageProps = await Component.getInitialProps(ctx);
+//   }
+//   pageProps.query = ctx.query;
+//   return { pageProps };
+// };
+
+// export default withData(MyApp)
+
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apollo';
 import '../styles/globals.css'
 import Page from '../components/Page';
 
-function MyApp({ Component, pageProps, err }) {
+const MyApp = ({ Component, pageProps }) => {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
   return (
-    <Page backgroundColor={pageProps?.backgroundColor}>
-      <Component err={err} {...pageProps} />
-    </Page>
+    <ApolloProvider client={apolloClient}>
+      <Page backgroundColor={pageProps?.backgroundColor}>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   )
 }
 
-export default MyApp
+export default MyApp;
