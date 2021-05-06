aeeb427461d603c5672e225c3e052071b7f5395b
Will Larry <will23larry@gmail.com>
2021-05-06 16:53:44 -0400

    Delete commented code from app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,3 @@
-// import { ApolloProvider } from '@apollo/client';
-// import '../styles/globals.css'
-// import Page from 'components/Page';
-// import withData from '../lib/utils/withData'
-
-// function MyApp({ Component, pageProps, err, apollo }) {
-//   console.log(apollo);
-  
-//   return (
-//     <ApolloProvider client={apollo}>
-//       <Page backgroundColor={pageProps?.backgroundColor}>
-//         <Component err={err} {...pageProps} />
-//       </Page>
-//     </ApolloProvider>
-
-//   )
-// }
-
-// MyApp.getInitialProps = async function ({ Component, ctx }) {
-//   let pageProps = {};
-//   if (Component.getInitialProps) {
-//     pageProps = await Component.getInitialProps(ctx);
-//   }
-//   pageProps.query = ctx.query;
-//   return { pageProps };
-// };
-
-// export default withData(MyApp)
-
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../lib/apollo';
